d24ae7dac1f6f90306ccce26c544236ce67f1f9b
Erik Mägi <erik.maegi@gmail.com>
2023-03-11 10:14:35 +0000

    Revert "updated c_cpp_properties.json"
    This reverts commit 510c5227a31655e1d9cf912c97de74ee4a39eec1.

--- merge_compile_commands.sh
+++
@@ -1,4 +0,0 @@
-printf '[' > compile_commands.json
-find ./build_release -type f -name 'compile_commands.json' -exec sh -c "cat {} | tail -n+2 | head -n-1 && printf ','" >> compile_commands.json \;
-sed -i '$s/.$//' compile_commands.json
-printf '\n]\n' >> compile_commands.json
